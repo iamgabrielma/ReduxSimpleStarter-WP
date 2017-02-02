cc8291104ead2fa8cc8243da8cc0277afae33e0c
Gabriel Maldonado Almendra <gma992@Gabriels-MacBook-Pro-2.local>
2017-02-02 13:41:25 +0100

    more reducing to app.js and index.js

--- src/components/app.js
+++ src/components/app.js
@@ -9,9 +9,22 @@ import React from 'react';
 // }
 
 var myApp = React.createClass({
+
 	render: function(){
+
+		var doingItWithStyle = {
+            color: 'red',
+            padding: 20,
+            width: 550,
+            margin: '0 auto',
+            fontFamily: 'Georgia',
+            fontSize: 22,
+            fontWeight: 'bold'
+		}
+
 		return(
-			<p>Hola!</p>
+			<p style={doingItWithStyle}>Hola!</p>
+			<Picture />		
 		);
 	}
 });
