935bd892319387181d404ce755298beea95eaf14
Gabriel Maldonado Almendra <gma992@Gabriels-MacBook-Pro-2.local>
2017-02-02 13:23:15 +0100

    reducing components to a single app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>React simple starter for WP - Check the console!</div>
+//     );
+//   }
+// }
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter for WP - Check the console!</div>
-    );
-  }
-}
+var myApp = React.createClass({
+	render: function(){
+		return(
+			<p>Hola!</p>
+		);
+	}
+});
+
+export default myApp;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom';
 //import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
-import PostDetails from './components/post_details';
+
+//import PostDetails from './components/post_details';
 //import PostDetailsTwo from './components/post_details_two';
-import Picture from './components/post_details_two';
+//import Picture from './components/post_details_two';
 //import Quote from './components/post_details_two';
 //import reducers from './reducers';
 
@@ -18,11 +19,13 @@ import Picture from './components/post_details_two';
 //   </Provider>
 //   , document.querySelector('.container'));
 //ReactDOM.render(<App/> , document.querySelector('.container'));
-ReactDOM.render(<App dataURL="https://www.osomstore.com/wp-json/wp/v2/posts/" />, document.querySelector('.container'));
+//ReactDOM.render(<App dataURL="https://www.osomstore.com/wp-json/wp/v2/posts/" />, document.querySelector('.container'));
 //ReactDOM.render(<SearchBar/> , document.querySelector('.post-details'));
-ReactDOM.render(<PostDetails/> , document.querySelector('.post-details'));
+//ReactDOM.render(<PostDetails/> , document.querySelector('.post-details'));
 //ReactDOM.render(<PostDetailsTwo/> , document.querySelector('.post-details-two'));
-ReactDOM.render(<Picture/> , document.querySelector('.post-details-two'));
+//ReactDOM.render(<Picture/> , document.querySelector('.post-details-two'));
 //ReactDOM.render(<Quote/> , document.querySelector('.post-details-two'));
 
-ReactDOM.render(<App dataURL="http://walden.dev/wp-json/wp/v2/posts" />, document.getElementById('content'));
+//ReactDOM.render(<App dataURL="http://walden.dev/wp-json/wp/v2/posts" />, document.getElementById('content'));
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
